91ab244892abe49c0b71cf14b15d8b982ec40616
luphin <sebaszegarra@gmail.com>
2024-09-01 14:41:40 -0400

    nuevas funciones y cambios del main

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from auth import create_user, authenticate_user
-from logger import log_info, log_warning
-from task_manager import create_task, update_task, delete_task, list_tasks, change_task_status
+from logger import log_info, log_warning, log_error
+from task_manager import create_task, update_task, delete_task, list_tasks, change_task_status, list_filed_tasks, check_and_update_overdue_tasks, list_tasks_with_indices, load_labels,load_statuses
+from datetime import datetime
 
 def main():
     while True:
+
         print("\nOpciones: [1] Iniciar sesión [2] Crear usuario [3] Salir")
         choice = input("Seleccione una opción: ")
 
@@ -15,34 +17,131 @@ def main():
                 log_info(f'Usuario {username} autenticado exitosamente')
                 print("Autenticado exitosamente")
 
+                # Verificar y actualizar tareas atrasadas al iniciar sesión
+                check_and_update_overdue_tasks(username)
+                                
+
                 while True:
-                    print(f"\nOpciones: [1] Crear tarea [2] Actualizar tarea [3] Eliminar tarea [4] Listar tareas [5] Cambiar estado de tarea [6] Cerrar sesión")
+                    print(f"\nTAREAS de ", username)
+                    print(f"\nPENDIENTES", "-"*20)
+                    list_tasks_with_indices(username, {"status": "pendiente"}, "label")
+                    print(f"\nEN PROGRESO", "-"*20)
+                    list_tasks_with_indices(username, {"status": "en progreso"}, "label")
+                    print(f"\nCOMPLETADAS", "-"*20)
+                    list_tasks_with_indices(username, {"status": "completada"}, "label")
+                
+                    print(f"\nOpciones:\n[1] Crear tarea\n[2] Actualizar tarea\n[3] Eliminar tarea\n[4] Filtrar tareas\n[5] Cambiar estado de tarea\n[6] Ver archivadas\n[7] Cerrar sesión")
                     task_choice = input("Seleccione una opción: ")
 
-                    if task_choice == '1':
-                        title = input("Título: ")
-                        description = input("Descripción: ")
-                        due_date = input("Fecha de vencimiento (YYYY-MM-DD): ")
-                        label = input("Etiqueta: ")
+                    if task_choice == '1': # Crear tarea
+                        title = input("Título: ").strip()
+                        if not title:
+                            log_warning(f"No se ingersó titulo para tarea. No se creó la tarea.")
+                            print("Aviso: El título de la tarea es obligatorio. No se creó la tarea.")
+                            continue
+
+                        description = input("Descripción: ").strip() or "-"
+                        due_date = input("Fecha de vencimiento (YYYY-MM-DD): ").strip() or "-"
+
+                        # Cargar etiquetas desde el archivo JSON
+                        labels = load_labels()
+
+                        # Seleccionar una etiqueta predefinida
+                        label = 'sin etiqueta'
+                        add_label = input("\n¿Desea agregar una etiqueta? (s/n): ").lower()
+
+                        if add_label == 's':
+                            print("\nSeleccione una etiqueta:")
+                            for i, lbl in enumerate(labels, start=1):
+                                print(f"[{i}] {lbl.capitalize()}")
+
+                            label_choice = input("Seleccione una opción: ")
+
+                            if label_choice.isdigit() and 1 <= int(label_choice) <= len(labels):
+                                label = labels[int(label_choice) - 1]
+                            else:
+                                print("Opción no válida. No se aplicará ninguna etiqueta.")
+                                label = 'sin etiqueta'  # Valor por defecto
+
                         create_task(title, description, due_date, label, username)
 
-                    elif task_choice == '2':
+                    elif task_choice == '2': # Actualizar datos
                         update_task(username)
 
-                    elif task_choice == '3':
+                    elif task_choice == '3': # Borrar tarea
                         delete_task(username)
 
-                    elif task_choice == '4':
+                    elif task_choice == '4': # Filtrar tareas
                         filters = {}
-                        status = input("Filtrar por estado (pendiente/en progreso/completada, dejar vacío para no filtrar): ")
-                        if status:
-                            filters['status'] = status
+                        
+                        # Preguntar si desea aplicar algún filtro
+                        apply_filters = input("\n¿Desea aplicar filtros? (s/n): ").lower()
+
+                        if apply_filters == 's':
+                            # Filtrado por título
+                            filter_by_title = input("\n¿Desea filtrar por título? (s/n): ").lower()
+
+                            if filter_by_title == 's':
+                                title = input("Ingrese el título a buscar: ")
+                                filters['title'] = {"$regex": title, "$options": "i"}  # Búsqueda por título (insensible a mayúsculas)
+                            else:
+                                # Filtrado por estado
+                                filter_by_status = input("\n¿Desea filtrar por estado? (s/n): ").lower()
+
+                                if filter_by_status == 's':
+                                    statuses = load_statuses()
+                                    print("\nSeleccione el estado:")
+                                    for i, status in enumerate(statuses, start=1):
+                                        print(f"[{i}] {status.capitalize()}")
+
+                                    status_choice = input("Seleccione una opción: ")
+
+                                    if status_choice.isdigit() and 1 <= int(status_choice) <= len(statuses):
+                                        filters['status'] = statuses[int(status_choice) - 1]
+                                    else:
+                                        print("Opción no válida. No se aplicará ningún filtro de estado.")
+                                
+                                # Filtrado por etiqueta
+                                filter_by_label = input("\n¿Desea filtrar por etiqueta? (s/n): ").lower()
+
+                                if filter_by_label == 's':
+                                    labels = load_labels()
+                                    print("\nSeleccione la etiqueta:")
+                                    for i, lbl in enumerate(labels, start=1):
+                                        print(f"[{i}] {lbl.capitalize()}")
+
+                                    label_choice = input("Seleccione una opción: ")
+
+                                    if label_choice.isdigit() and 1 <= int(label_choice) <= len(labels):
+                                        filters['label'] = labels[int(label_choice) - 1]
+                                    else:
+                                        print("Opción no válida. No se aplicará ningún filtro de etiqueta.")
+                                
+                                # Filtrado por rango de fechas desde la fecha actual
+                                filter_by_date_range = input("\n¿Desea filtrar por un rango de fechas desde hoy? (s/n): ").lower()
+
+                                if filter_by_date_range == 's':
+                                    end_date = input("Ingrese la fecha de fin (YYYY-MM-DD): ")
+                                    current_date = datetime.now().strftime('%Y-%m-%d')  # Obtener la fecha actual
+
+                                    try:
+                                        filters['due_date'] = {"$gte": current_date, "$lte": end_date}
+                                    except Exception as e:
+                                        print("Formato de fecha inválido.")
+                                        log_warning(f"Formato de fecha inválido al filtrar por rango de fechas: {e}")
+
+                        # Eliminar claves nulas
+                        filters = {k: v for k, v in filters.items() if v is not None}
+
                         list_tasks(username, filters)
 
-                    elif task_choice == '5':
+                    elif task_choice == '5': # Cambiar estado
                         change_task_status(username)
 
-                    elif task_choice == '6':
+                    elif task_choice == '6':  # Tareas completadas/archivadas
+                        list_filed_tasks(username)
+
+                    elif task_choice == '7': # Cerrar sesion
                         print("Cerrando sesión...")
                         break
 
@@ -66,6 +165,7 @@ def main():
 
         else:
             print("Opción no válida.")
+
 
 if __name__ == "__main__":
     main()
--- task_manager.py
+++ task_manager.py
@@ -1,11 +1,16 @@
 from pymongo import MongoClient
 from datetime import datetime
 from logger import log_info, log_warning, log_error
+from datetime import datetime
+import json
 
+# Conectar con base de datos
 client = MongoClient('mongodb://localhost:27017/')
 db = client['task_manager']
-tasks_collection = db['tasks']
+tasks_collection = db['tasks'] # Colección para tareas
 
+
+# Crear tarea  
 def create_task(title, description, due_date, label, username):
     task = {
         "title": title,
@@ -23,41 +28,63 @@ def create_task(title, description, due_date, label, username):
     except Exception as e:
         log_error(f'Error al crear tarea: {e}')
 
-
+# Actualizar datos de tareas
 def update_task(username):
     tasks = list_tasks_with_indices(username)
     
     if not tasks:
         return
 
+    labels = load_labels()  # Cargar las etiquetas desde el archivo
+
     try:
         task_num = int(input("Seleccione el número de la tarea que desea actualizar: "))
         selected_task = tasks[task_num - 1]
         
         updates = {}
+        
+        # Actualización de título
         title = input(f"Nuevo título (actual: {selected_task['title']}, dejar vacío para no cambiar): ")
         if title:
             updates['title'] = title
+        
+        # Actualización de descripción
         description = input(f"Nueva descripción (actual: {selected_task['description']}, dejar vacío para no cambiar): ")
         if description:
             updates['description'] = description
+        
+        # Actualización de fecha de vencimiento
         due_date = input(f"Nueva fecha de vencimiento (actual: {selected_task['due_date']}, dejar vacío para no cambiar): ")
         if due_date:
             updates['due_date'] = due_date
-        label = input(f"Nueva etiqueta (actual: {selected_task['label']}, dejar vacío para no cambiar): ")
-        if label:
-            updates['label'] = label
         
-        tasks_collection.update_one({'_id': selected_task['_id']}, {'$set': updates})
-        log_info(f'Tarea actualizada: {selected_task["_id"]} por el usuario {username}')
-        print("Tarea actualizada exitosamente.")
+        # Actualización de etiqueta con opciones predefinidas
+        print(f"Etiqueta actual: {selected_task['label']}")
+        print("Seleccione una nueva etiqueta (dejar vacío para no cambiar):")
+        
+        for i, label in enumerate(labels, start=1):
+            print(f"[{i}] {label}")
+
+        label_choice = input("Seleccione una opción: ")
+        
+        if label_choice.isdigit() and 1 <= int(label_choice) <= len(labels):
+            updates['label'] = labels[int(label_choice) - 1]
+        
+        # Actualización en la base de datos
+        if updates:
+            tasks_collection.update_one({'_id': selected_task['_id']}, {'$set': updates})
+            log_info(f'Tarea actualizada: {selected_task["_id"]} por el usuario {username}')
+            print("Tarea actualizada exitosamente.")
+        else:
+            print("No se realizaron cambios.")
+
     except (ValueError, IndexError):
         print("Selección inválida.")
         log_warning(f'Selección inválida en la actualización de tarea para el usuario {username}.')
     except Exception as e:
         log_error(f'Error al actualizar tarea para el usuario {username}: {e}')
 
-
+# Borrar tarea
 def delete_task(username):
     try:
         # Listar tareas con índices
@@ -92,8 +119,7 @@ def delete_task(username):
         log_error(f'Error al eliminar tarea para el usuario {username}: {e}')
         print("Ocurrió un error al eliminar la tarea.")
 
-
-
+# Listar tareas
 def list_tasks(username, filters=None):
     try:
         query = filters if filters else {}
@@ -123,8 +149,7 @@ def list_tasks(username, filters=None):
         log_error(f'Error al listar tareas para el usuario {username}: {e}')
         print("Ocurrió un error al listar las tareas.")
 
-
-
+# Cambiar estado de las tareas
 def change_task_status(username):
     try:
         # Listar tareas con índices
@@ -132,6 +157,9 @@ def change_task_status(username):
         
         if not tasks:
             return  # Salir si no hay tareas para cambiar el estado
+
+        # Cargar los estados desde el archivo JSON
+        statuses = load_statuses()
 
         # Solicitar al usuario que seleccione una tarea para cambiar su estado
         task_num = int(input("Seleccione el número de la tarea cuyo estado desea cambiar: "))
@@ -146,9 +174,18 @@ def change_task_status(username):
         
         # Mostrar el estado actual y solicitar el nuevo estado
         print(f"Estado actual de '{selected_task['title']}': {selected_task['status']}")
-        new_status = input("Ingrese el nuevo estado (pendiente, en progreso, completada): ").lower()
+        
+        # Mostrar las opciones de estados disponibles
+        print("Seleccione un nuevo estado:")
+        for i, status in enumerate(statuses, start=1):
+            print(f"[{i}] {status.capitalize()}")
 
-        if new_status in ['pendiente', 'en progreso', 'completada']:
+        status_choice = input("Seleccione una opción: ")
+        
+        # Validar la elección del estado
+        if status_choice.isdigit() and 1 <= int(status_choice) <= len(statuses):
+            new_status = statuses[int(status_choice) - 1]
+            # Actualizar el estado de la tarea si no es "completada"
             tasks_collection.update_one({'_id': task_id}, {'$set': {'status': new_status}})
             log_info(f'Estado de tarea actualizado: {task_id} por el usuario {username} a {new_status}')
             print("Estado de la tarea actualizado exitosamente.")
@@ -162,9 +199,74 @@ def change_task_status(username):
         log_error(f'Error al cambiar el estado de la tarea para el usuario {username}: {e}')
         print("Ocurrió un error al cambiar el estado de la tarea.")
 
+# Taeas archivadas
+def list_filed_tasks(username):
+    try:
+        while True:
+            # Listar tareas archivadas con índice, solo con estado "completada"
+            archived_tasks = list_tasks_with_indices(username, {"status": "completada"})
+            
+            if not archived_tasks:
+                return  # Salir si no hay tareas archivadas
 
+            # Opciones de eliminación
+            print("\nOpciones de eliminación:")
+            print("[1] Eliminar todas las tareas archivadas")
+            print("[2] Eliminar una tarea específica")
+            print("[3] Eliminar un rango de tareas por índice")
+            print("   (Dejar vacío para salir)")
 
-def list_tasks_with_indices(username, filters=None):
+            choice = input("Seleccione una opción: ").strip()
+
+            if choice == "":
+                print("Saliendo del menú de eliminación de tareas archivadas.")
+                break  # Salir del bucle y la función
+
+            elif choice == "1":
+                confirmation = input("¿Está seguro de que desea eliminar TODAS las tareas archivadas? (s/n): ").lower().strip()
+                if confirmation == 's':
+                    tasks_collection.delete_many({"status": "completada", "username": username})
+                    log_info(f'Todas las tareas completadas fueron eliminadas para el usuario {username}.')
+                    print("Todas las tareas completadas han sido eliminadas exitosamente.")
+                else:
+                    print("Eliminación cancelada.")
+
+            elif choice == "2":
+                try:
+                    task_num = int(input("Seleccione el número de la tarea que desea eliminar: ").strip())
+                    if 1 <= task_num <= len(archived_tasks):
+                        selected_task = archived_tasks[task_num - 1]
+                        tasks_collection.delete_one({'_id': selected_task['_id']})
+                        log_info(f'Tarea completada eliminada: {selected_task["_id"]} por el usuario {username}')
+                        print("Tarea completada eliminada exitosamente.")
+                    else:
+                        print("Selección inválida. Intente nuevamente.")
+                except ValueError:
+                    print("Entrada no válida. Intente nuevamente.")
+
+            elif choice == "3":
+                try:
+                    start_index = int(input("Ingrese el índice inicial del rango de tareas a eliminar: ").strip())
+                    end_index = int(input("Ingrese el índice final del rango de tareas a eliminar: ").strip())
+                    if 1 <= start_index <= end_index <= len(archived_tasks):
+                        task_ids_to_delete = [archived_tasks[i - 1]['_id'] for i in range(start_index, end_index + 1)]
+                        tasks_collection.delete_many({'_id': {'$in': task_ids_to_delete}})
+                        log_info(f'Tareas completadas eliminadas por rango para el usuario {username}')
+                        print("Tareas completadas eliminadas exitosamente.")
+                    else:
+                        print("Rango de índices inválido. Intente nuevamente.")
+                except ValueError:
+                    print("Entrada no válida. Intente nuevamente.")
+
+            else:
+                print("Opción no válida. Intente nuevamente.")
+            
+    except Exception as e:
+        log_error(f'Error al listar y eliminar tareas archivadas para el usuario {username}: {e}')
+        print("Ocurrió un error al realizar la operación.")
+
+# Lista de tareas abreviada, utilizada en delete, update y modificar estado. Es una vista simple de las tareas.
+def list_tasks_with_indices(username, filters=None, mostrar="status"):
     try:
         # Asegurarse de que filters sea un diccionario, si no se pasa nada se crea un diccionario vacío
         query = filters if filters else {}
@@ -172,16 +274,23 @@ def list_tasks_with_indices(username, filters=None):
 
         tasks = tasks_collection.find(query)
 
-        task_list = list(tasks)  # Convertimos el cursor en una lista
+        task_list = list(tasks)  # Convertir el cursor en una lista
 
         if len(task_list) == 0:
             print("No se encontraron tareas.")
             log_info(f"No se encontraron tareas para el usuario {username}.")
             return []
 
-        # Mostrar tareas con índices
-        for i, task in enumerate(task_list, start=1):
-            print(f"{i}. Título: {task['title']}, Descripción: {task['description']}, Estado: {task['status']}")
+        if mostrar == "status":
+            # Mostrar tareas con índices
+            for i, task in enumerate(task_list, start=1):
+                print(f"{i}. Título: {task['title']}, Descripción: {task['description']}, Estado: {task['status']}, Fecha de vencimiento: {task['due_date']}")
+        
+        elif mostrar == "label":
+            # Mostrar tareas con índices
+            for i, task in enumerate(task_list, start=1):
+                print(f"{i}. Título: {task['title']}, Descripción: {task['description']}, Etiqueta: {task['label']}, Fecha de vencimiento: {task['due_date']}")
+
 
         return task_list
 
@@ -190,5 +299,46 @@ def list_tasks_with_indices(username, filters=None):
         print("Ocurrió un error al listar las tareas.")
         return []
 
+# Función para verificar y actualizar el estado de las tareas atrasadas
+def check_and_update_overdue_tasks(username):
+    try:
+        current_date = datetime.now().date()
+        tasks = tasks_collection.find({"username": username, "status": {"$ne": "completada"}})
+        
+        for task in tasks:
+            due_date = datetime.strptime(task['due_date'], "%Y-%m-%d").date()
+            if due_date < current_date and task['status'] != "atrasado":
+                tasks_collection.update_one(
+                    {"_id": task["_id"]},
+                    {"$set": {"status": "atrasado"}}
+                )
+                log_info(f"Estado de la tarea '{task['title']}' actualizado a 'atrasado' para el usuario {username}.")
+                
+    except Exception as e:
+        log_error(f"Error al verificar y actualizar tareas atrasadas para el usuario {username}: {e}")
 
+# Función para cargar etiquetas desde un archivo JSON
+def load_labels(filename="labels.json"):
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+            return data.get("labels", [])
+    except FileNotFoundError:
+        print("Archivo de etiquetas no encontrado. Usando etiquetas por defecto.")
+        return ["urgente", "personal", "trabajo", "proyecto", "sin etiqueta"]
+    except json.JSONDecodeError:
+        print("Error al leer el archivo de etiquetas. Usando etiquetas por defecto.")
+        return ["urgente", "personal", "trabajo", "proyecto", "sin etiqueta"]
 
+# Función para cargar estados desde un archivo JSON
+def load_statuses(filename="statuses.json"):
+    try:
+        with open(filename, "r") as file:
+            data = json.load(file)
+            return data.get("statuses", [])
+    except FileNotFoundError:
+        print("Archivo de estados no encontrado. Usando estados por defecto.")
+        return ["pendiente", "en progreso", "completada"]
+    except json.JSONDecodeError:
+        print("Error al leer el archivo de estados. Usando estados por defecto.")
+        return ["pendiente", "en progreso", "completada"]
